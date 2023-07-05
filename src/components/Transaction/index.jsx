7d05e2c293bd68f611d1449005a1cc75fb29a136
Matheus Vespasiano <vespasianomatheus@gmail.com>
2023-07-05 12:13:42 -0300

    removendo erro styled-components

--- src/components/Transaction/index.jsx
+++ src/components/Transaction/index.jsx
@@ -1,17 +1,23 @@
+import { StyleSheetManager } from "styled-components"
 import * as S from "./styles"
+
 import { TrendingUp, TrendingDown } from "lucide-react"
+
 export function Transaction({ item }) {
+  const expense = item.expense
   return (
-    <S.Container>
-      <S.Desc>
-        {item?.expense ? (
-          <TrendingDown color="#EB5B64" />
-        ) : (
-          <TrendingUp color="#0AE360" />
-        )}
-        <span>{item.desc}</span>
-      </S.Desc>
-      <S.Amount expense={item?.expense ? 1 : 0}>{item.amount}</S.Amount>
-    </S.Container>
+    <StyleSheetManager shouldForwardProp={prop => prop !== "expense"}>
+      <S.Container>
+        <S.Desc>
+          {item?.expense ? (
+            <TrendingDown color="#EB5B64" />
+          ) : (
+            <TrendingUp color="#0AE360" />
+          )}
+          <span>{item.desc}</span>
+        </S.Desc>
+        <S.Amount expense={expense ? 1 : 0}>{item.amount}</S.Amount>
+      </S.Container>
+    </StyleSheetManager>
   )
 }
